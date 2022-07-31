a30043f90aaeef64cec284b4df1030bcb1acd8f8
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-07-31 18:45:40 +0200

    feat: bookmark multiple selected tabs fix: keyboard shotcut not working

--- background.js
+++ background.js
@@ -3,8 +3,16 @@
 const manifest = browser.runtime.getManifest();
 const extname = manifest.name;
 
+
 browser.menus.create({
-	id: extname,
+	id: extname + "_tabs",
+	title: "Bookmark Placer",
+	contexts: ["tab"],
+	onclick: bookmark // function(info/*, tab*/) {}
+});
+
+browser.menus.create({
+	id: extname + "_bookmarks",
 	title: "Copy Bookmark Folder Id",
 	contexts: ["bookmark"],
 	visible: false,
@@ -25,33 +33,41 @@ browser.menus.onShown.addListener(async function(info/*, tab*/) {
 	if(info.bookmarkId) {
         const bookmarkTreeNode = (await browser.bookmarks.get(info.bookmarkId))[0];
 		if(bookmarkTreeNode.url) {
-			await browser.menus.update(extname, {visible: false});
+			await browser.menus.update(extname+"_bookmarks", {visible: false});
 		}else{
-			await browser.menus.update(extname, {visible: true});
+			await browser.menus.update(extname+"_bookmarks", {visible: true});
 		}
 	}
 	browser.menus.refresh();
 });
 
 
-browser.browserAction.onClicked.addListener(async (tab) => {
-    // 1. get fromm regexs + bookmarkIds from storage
+browser.browserAction.onClicked.addListener(bookmark);
+
+async function bookmark() {
+
+    // operate on highlighted tabs
+    const tabs = await browser.tabs.query({ currentWindow:true, highlighted: true, hidden: false });
 
     let store = {};
 
 	try {
 		store = await browser.storage.local.get('selectors');
+        if(typeof store === 'undefined'){
+            store = {};
+        }
 	}catch(e){
 		console.error('error', 'access to rules storage failed');
-		return;
+        store = {};
 	}
 
-	if ( typeof store.selectors !== 'object' ) {
-		console.error('error', 'rules selectors not available');
-		return;
-	}
+    if( typeof store.selectors === 'undefined' ){
+        store.selectors = [];
+    }
 
-    const bookmarkId = (function() {
+    let bookmarkId = null;
+    for(let tab of tabs) {
+        bookmarkId = null;
         for(let selector of store.selectors) {
             // check activ
             if(typeof selector.activ === 'boolean') {
@@ -63,7 +79,8 @@ browser.browserAction.onClicked.addListener(async (tab) => {
                         if((new RegExp(selector.url_regex)).test(tab.url)){
                             if ( typeof selector.bookmarkId === 'string' ) {
                                 if ( selector.bookmarkId !== '' ) {
-                                        return selector.bookmarkId;
+                                        bookmarkId = selector.bookmarkId;
+                                        break;
                                     }
                                 }
                             }
@@ -72,31 +89,13 @@ browser.browserAction.onClicked.addListener(async (tab) => {
                 }
             }
         }
-        return null;
-    }());
-
-    /*
-    try {
-        const bookmarkTreeNode = await browser.bookmarks.get(bookmarkId);
-    }catch(e){
-        console.error(e);
-        // bookmark with id doent exist
-        return;
+        let createdetails = {
+            title: tab.title,
+            url: tab.url
+        }
+        if(bookmarkId !== null){
+            createdetails.parentId = bookmarkId;
+        }
+        browser.bookmarks.create(createdetails);
     }
-
-    if(!bookmarkTreeNode.children){
-        // not a folder ref. https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/BookmarkTreeNode
-        return;
-    }
-    */
-    let createdetails = {
-        title: tab.title,
-        url: tab.url
-    }
-    if(bookmarkId !== null){
-        createdetails.parentId = bookmarkId;
-    }
-
-    browser.bookmarks.create(createdetails);
-
-});
+}
